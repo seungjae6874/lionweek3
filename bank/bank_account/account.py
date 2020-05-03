767d4601f2a4682fd86c3b328d2a2f85e3b76293
user <98snadpragon@gmail.com>
2020-05-04 02:51:54 +0900

    이름에서 숫자 제외, 전체에서 음수 제외, 계좌번호 int로 수정했습니다!

--- bank/bank_account/account.py
+++ bank/bank_account/account.py
@@ -6,17 +6,22 @@ class Account():
         #계좌번호 정수만 입력
         print("\n======계좌개설======\n")
         try:
-            account_num = input("계좌번호: ") #계좌는 str
-            try:
-                name = input("이름: ")
+            account_num = int(input("계좌번호: ")) #계좌는 str->int로 모두 수정
+            if account_num < 0:
+                raise NotImplementedError #계좌번호, 입금, 출금에서 전부 음수 입력X
+            name = input("이름: ")
+            if name.isdigit() == False: #이름에 숫자X
                 self.name_list[account_num] = name
                 try:
-                    self.account_list[account_num] = int(input("예금: "))
+                    first_deposit = int(input("예금: "))
+                    if first_deposit < 0:
+                        raise NotImplementedError
+                    self.account_list[account_num] = first_deposit
                     print("##계좌개설을 완료하였습니다##")
                     print("====================")
                 except:
                     print("금액 입력이 올바르지 않습니다.")
-            except:
+            else:
                 print("이름 입력이 올바르지 않습니다.")
         except:
             print("계좌번호 입력이 올바르지 않습니다.")
@@ -35,16 +40,19 @@ class Account():
         #계좌를 입력받고
         #해당 계좌에 돈을 입금 해당 계좌 : +=금액
         print("======입금하기======")
-        checkaccount = input("입금하실 계좌번호를 입력해주세요: ")
+        checkaccount = int(input("입금하실 계좌번호를 입력해주세요: "))
         if checkaccount in self.account_list.keys():
             print("계좌이름:", self.name_list[checkaccount])
             print("계좌잔고:", self.account_list[checkaccount], "원")
             balance = int(input("입금하실 금액을 입력해주세요: "))
             #money = self.account_list[checkaccount]+balance
-            self.account_list[checkaccount] += balance
-            print('\n##계좌잔고: ', self.account_list[checkaccount], "원##")
-            print("##입금이 완료되었습니다##")
-            print("====================")
+            if balance >= 0:
+                self.account_list[checkaccount] += balance
+                print('\n##계좌잔고: ', self.account_list[checkaccount], "원##")
+                print("##입금이 완료되었습니다##")
+                print("====================")
+            else:
+                print("금액 입력이 올바르지 않습니다.") #음수 입력X
         else:
             print(self.account_list)
             print("해당 계좌가 존재하지 않습니다.")
@@ -55,13 +63,15 @@ class Account():
         #만약 출금 금액이 잔액보다 크면 불가
         print("======출금하기======")
         try:
-            checkaccount = input("출금하실 계좌번호를 입력해주세요: ")
+            checkaccount = int(input("출금하실 계좌번호를 입력해주세요: "))
             if checkaccount in self.account_list.keys():
                 print("계좌이름:", self.name_list[checkaccount])
                 print("계좌잔고:", self.account_list[checkaccount], "원")
                 withdraw = int(input("출금하실 금액을 입력해주세요: "))
                 if withdraw > self.account_list[checkaccount]: #출금 초과
                     print("잔액 부족")
+                elif withdraw < 0:
+                    print("금액 입력이 올바르지 않습니다.") #음수 입력X
                 else:
                     self.account_list[checkaccount] -= withdraw
                     print('\n##계좌잔고: ', self.account_list[checkaccount], "원##")
